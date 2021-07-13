cf9db53476c32ef913ebf7086d7f1adc869c4fa9
Neeraj Garg <41155992+neerajgarg@users.noreply.github.com>
2021-07-13 18:30:46 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       exec_mode: 'cluster',
       name: 'testingFE1',
-      cwd: '/home/ubuntu/TestDir/FrontEnd',
+      cwd: '/home/ubuntu/Test-dir/Business_search_app/FrontEnd',
       script: './node_modules/nuxt/bin/nuxt.js',
       port: 3030,
       instances: 'max',
