0d3f5203f2d5fbc24d75a5e6ae745a89165d6a3b
abirkhan04 <khanabir8536@gmail.com>
2021-07-14 23:13:22 +0600

    [] ecosystem file

---
+++ FrontEnd/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      exec_mode: 'cluster',
+      name: 'testingFE1',
+      cwd: '/home/ubuntu/Test-dir/Business_search_app/FrontEnd',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      host: '0.0.0.0',
+      port: 9000,
+      instances: 'max',
+      args: 'stremotart'
+    },
+
+  ]
+};
