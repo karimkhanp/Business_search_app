f104e9e1004e70e2a99a22e59176719621b75d33
abirkhan04 <khanabir8536@gmail.com>
2021-07-15 08:00:56 +0600

    [] updated with git setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,9 @@ module.exports = {
       exec_mode: 'cluster',
       name: 'testingFE1',
       cwd: '/home/ubuntu/Test-dir/Business_search_app/FrontEnd',
-      script: './node_modules/nuxt/bin/nuxt.js',
+      script: './FrontEnd/node_modules/nuxt/bin/nuxt.js',
+      ref  : "origin/business_search_app1",
+      repo: "https://github.com/karimkhanp/Business_search_app.git",
       port: 3030,
       instances: 'max',
       args: 'start'
