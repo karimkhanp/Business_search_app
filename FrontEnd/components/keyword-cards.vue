002f8171f57efcb6b2a0537184bc8627f182d6b8
abirkhan04 <khanabir8536@gmail.com>
2021-07-26 18:59:17 +0600

    [] A fixation.

--- FrontEnd/components/keyword-cards.vue
+++ FrontEnd/components/keyword-cards.vue
@@ -4,7 +4,7 @@
             <div v-if="jobTitle">Job Title: <span>{{jobTitle}}</span><a v-on:click="remove(`jobTitle`)">x</a> </div>
             <div v-if="country && country.length>0">Country: <span>{{country.join(',')}}</span><a v-on:click="remove(`country`)">x</a></div>
             <div v-if="city">City: <span>{{city}}</span><a v-on:click="remove(`city`)">x</a> </div>
-            <div v-if="employee">Company Size: <span>{{employee.join(', ')}}</span><a v-on:click="remove(`employee`)">x</a></div>
+            <div v-if="employee && employee.length > 0">Company Size: <span>{{employee.join(', ')}}</span><a v-on:click="remove(`employee`)">x</a></div>
         </div>
 </template>
 <script>
