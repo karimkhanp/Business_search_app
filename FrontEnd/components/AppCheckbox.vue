eb580880a7b3647e38ff62771424b97165bf64eb
abirkhan04 <khanabir8536@gmail.com>
2021-08-03 16:53:02 +0600

    [] comment removed.

--- FrontEnd/components/AppCheckbox.vue
+++ FrontEnd/components/AppCheckbox.vue
@@ -20,8 +20,6 @@ export default {
 </script>
 
 <style scoped>
-
-/* The container */
 .container {
   display: block;
   position: absolute;
@@ -36,7 +34,6 @@ export default {
   user-select: none;
 }
 
-/* Hide the browser's default checkbox */
 .container input {
   position: absolute;
   opacity: 0;
@@ -45,7 +42,6 @@ export default {
   width: 0;
 }
 
-/* Create a custom checkbox */
 .checkmark {
   position: absolute;
   top: 10px;
@@ -57,12 +53,11 @@ export default {
   border-radius: 2px;
 }
 
-/* On mouse-over, add a grey background color */
 .container:hover input ~ .checkmark {
   background-color: white;
 }
 
-/* When the checkbox is checked, add a blue background */
+
 .container input:checked ~ .checkmark {
   background-color: #2196F3;
   position: absolute;
@@ -70,19 +65,18 @@ export default {
   left: 15px;
 }
 
-/* Create the checkmark/indicator (hidden when not checked) */
+
 .checkmark:after {
   content: "";
   position: absolute;
   display: none;
 }
 
-/* Show the checkmark when checked */
+
 .container input:checked ~ .checkmark:after {
   display: block;
 }
 
-/* Style the checkmark/indicator */
 .container .checkmark:after {
   left: 6px;
   top: 2px;
